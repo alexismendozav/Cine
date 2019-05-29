70eeb0e9eea2abecdb454ea54cf0a658938e9f1f
Alexis Mendoza Vallarta <alxismendoza@gmail.com>
2019-05-28 20:54:22 -0500

    Se agrego un select para la busqueda por dia y ya se muestran os datos de la pelicula a exepcion de las horas

--- app/controllers/CarteleraController.php
+++ app/controllers/CarteleraController.php
@@ -14,6 +14,9 @@ class CarteleraController extends \Phalcon\Mvc\Controller
         $proyecciones= Proyecciones::find();
         $this->view->proyecciones = $proyecciones;
 
+        $categorias= Categorias::find();
+        $this->view->categorias = $categorias;
+
         
         
     }

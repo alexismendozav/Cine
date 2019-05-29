d73f33e838c0590cd620f3d04deed9a6ac9d74c3
Valeria Ladino Núñez <valeria.nulad@gmail.com>
2019-05-29 03:19:43 -0500

    modificaciones en la vista peliculas

--- app/controllers/PeliculasController.php
+++ app/controllers/PeliculasController.php
@@ -7,6 +7,19 @@ class PeliculasController extends \Phalcon\Mvc\Controller
     {
         
     }
+    public function datatableAction()
+    {
+      if( $this->request->isAjax() )
+      {
+        $this->view->disable();
+        $pelicula = Peliculas::find();
+        //Mandar los datos a la vista
+        $this->response->setJsonContent(["data"=>$pelicula]);
+        $this->response->setStatusCode(200, "OK");
+        $this->response->send();
+      }
+
+    }
 
 }
 
